a7694ed90da954ac6ee66cd133c6137e821a18f1
KylieGursky <kyliemgursky@gmail.com>
2020-05-07 12:09:38 -0700

    collecting ducks

--- avatar/sketch.js
+++ avatar/sketch.js
@@ -3,6 +3,15 @@ let rowboat;
 let truck;
 let me;
 let ducks;
+let duck1;
+let duck2;
+let duck3;
+let duck4;
+let duck5;
+let duck6;
+let duck7;
+let duck8;
+let duck9;
 
 function preload() {
 farmhouse = loadImage('pictures/farmhouse.png')
@@ -13,7 +22,16 @@ truck = loadImage('pictures/truck.png')
 function setup() {
   createCanvas(800,400);
   me = new Avatar(width/2,300,3)
-  ducks = new Duck(780,98)
+  ducks = new Duck(650,250)
+  duck1 = new Duck(100,300)
+  duck2 = new Duck(400,200)
+  duck3 = new Duck(550,100)
+  duck4 = new Duck(300,200)
+  duck5 = new Duck(680,20)
+  duck6 = new Duck(200,350)
+  duck7 = new Duck(444,333)
+  duck8 = new Duck(350,80)
+  duck9 = new Duck(80,250)
 }
 
 function draw() {
@@ -37,6 +55,27 @@ function draw() {
 
   me.drawMe();
   me.moveMe();
+
+  ducks.collectDuck();
+  ducks.drawDuck();
+  duck1.collectDuck();
+  duck1.drawDuck();
+  duck2.collectDuck();
+  duck2.drawDuck();
+  duck3.collectDuck();
+  duck3.drawDuck();
+  duck4.collectDuck();
+  duck4.drawDuck();
+  duck5.collectDuck();
+  duck5.drawDuck();
+  duck6.collectDuck();
+  duck6.drawDuck();
+  duck7.collectDuck();
+  duck7.drawDuck();
+  duck8.collectDuck();
+  duck8.drawDuck();
+  duck9.collectDuck();
+  duck9.drawDuck();
 
   field(25,270)
   field(50,270)
@@ -75,7 +114,6 @@ function draw() {
   field(125,320)
   field(150,320)
 
- ducks.drawDuck();
 
 }
 
@@ -133,15 +171,19 @@ class Duck{
     fill(255,219,77)
     noStroke()
     ellipse(this.x,this.y,10,8)
+    ellipse(this.x-3,this.y-4.5,6,6)
+    fill("orange")
+    ellipse(this.x-6,this.y-4.5,3,2)
   }
 
   moveDuck(){
+
   }
 
   collectDuck(){
-    if (this.x >= me.x-15, this.x<= me.x+15, this.y > me.y-40, this.y < me.y+40){
-      this.x = this.x-680;
-      this.y = this.y-78;
+    if (this.x >= me.x-15 && this.x<= me.x+15 && this.y > me.y-20 && this.y < me.y+20){
+      this.x = 20;
+      this.y = 20;
     }
   }
 }
